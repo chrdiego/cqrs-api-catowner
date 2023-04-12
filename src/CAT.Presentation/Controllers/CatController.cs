508ebb2b4d3856abc18d851d876a1e43a91be1eb
Diego <wchirdo@gmail.com>
2023-04-12 16:23:27 -0300

    Init commit

---
+++ src/CAT.Presentation/Controllers/CatController.cs
@@ -0,0 +1,21 @@
+﻿using CAT.Presentation.Infrastructure;
+using MediatR;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CAT.Presentation.Controllers
+{
+    public class CatController : ApiBaseController
+    {
+        public CatController(ISender sender) : base(sender)
+        {
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            //this.Sender.Send();
+            return this.Ok();
+        }
+    }
+}
---
+++ src/CAT.Presentation/Infrastructure/ApiBaseController.cs
@@ -0,0 +1,23 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Hosting.Server;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CAT.Presentation.Infrastructure
+{
+    [Route("api/[controller]")]
+    public class ApiBaseController : ControllerBase
+    {
+        protected ApiBaseController(ISender sender)
+        {
+            Sender = sender;
+        }
+
+        protected ISender Sender { get; }
+
+        //protected IActionResult BadRequest(Error error) => BadRequest(new ApiErrorResponse(new[] { error }));
+
+        //protected new IActionResult Ok(object value) => base.Ok(value);
+
+        //protected new IActionResult NotFound() => NotFound("The requested resource was not found.");
+    }
+}
